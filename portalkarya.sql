cdd9b561a3cbc5c42f40e6f84ad22271859fd380
nurlaelakhasannah <nurlaelakhsnnh12@gmail.com>
2019-12-15 23:12:05 +0700

    New Database

--- portalkarya.sql
+++ portalkarya.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2019 at 01:53 AM
+-- Generation Time: Dec 15, 2019 at 04:48 PM
 -- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- PHP Version: 7.2.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,21 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id_admin` int(3) NOT NULL,
-  `nama` varchar(30) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `foto` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `project`
 --
 
@@ -54,26 +39,29 @@ CREATE TABLE `project` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `tbuser`
 --
 
-CREATE TABLE `user` (
-  `id_user` int(3) NOT NULL,
-  `nama` varchar(30) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  `foto` varchar(1000) NOT NULL
+CREATE TABLE `tbuser` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `level` enum('admin','user') NOT NULL,
+  `foto` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbuser`
+--
+
+INSERT INTO `tbuser` (`id_user`, `nama`, `email`, `username`, `password`, `level`, `foto`) VALUES
+(1, 'Nurlaela Khasannah', 'nur@gmail.com', 'ela', 'ela', 'admin', NULL);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Indexes for table `project`
@@ -82,10 +70,20 @@ ALTER TABLE `project`
   ADD PRIMARY KEY (`id_project`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `tbuser`
 --
-ALTER TABLE `user`
+ALTER TABLE `tbuser`
   ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbuser`
+--
+ALTER TABLE `tbuser`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
